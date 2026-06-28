adb1908c783e670d02cb37c42a77fdc062632402
Sourabh Thakur <aryansngh321@gmail.com>
2026-06-28 11:54:26 +0530

    Polish header UI and mobile navigation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,43 +12,46 @@ export default function Footer() {
             <p className="site-footer__tag">
               Himalayan heritage foods, sourced at origin and brought to your table unhurried.
             </p>
+            <div className="site-footer__mark">
+              <MillWheel size={54} spinning={false} />
+            </div>
           </div>
 
           <div className="site-footer__col">
             <p className="site-footer__heading">Shop</p>
-            <ul>
-              <li><Link to="/shop">All Products</Link></li>
-              <li><Link to="/shop/honey">Raw Honey</Link></li>
-              <li><Link to="/shop">Gift Boxes</Link></li>
+            <ul className="site-footer__links">
+              <li className="site-footer__item"><Link to="/shop">All Products</Link></li>
+              <li className="site-footer__item"><Link to="/shop/honey">Raw Honey</Link></li>
+              <li className="site-footer__item"><Link to="/shop">Gift Boxes</Link></li>
             </ul>
           </div>
 
           <div className="site-footer__col">
             <p className="site-footer__heading">About</p>
-            <ul>
-              <li><Link to="/about">Our Story</Link></li>
-              <li><Link to="/producers">Producers</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
+            <ul className="site-footer__links">
+              <li className="site-footer__item"><Link to="/about">Our Story</Link></li>
+              <li className="site-footer__item"><Link to="/producers">Producers</Link></li>
+              <li className="site-footer__item"><Link to="/contact">Contact</Link></li>
             </ul>
           </div>
 
           <div className="site-footer__col">
             <p className="site-footer__heading">Support</p>
-            <ul>
-              <li><Link to="/shipping">Shipping</Link></li>
-              <li><Link to="/privacy">Privacy Policy</Link></li>
-              <li><Link to="/terms">Terms of Service</Link></li>
-              <li><a href="https://wa.me/919882238158" target="_blank" rel="noopener noreferrer">WhatsApp Us</a></li>
-              <li><a href="https://instagram.com/gharrat.in" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+            <ul className="site-footer__links">
+              <li className="site-footer__item"><Link to="/shipping">Shipping</Link></li>
+              <li className="site-footer__item"><Link to="/privacy">Privacy Policy</Link></li>
+              <li className="site-footer__item"><Link to="/terms">Terms of Service</Link></li>
+              <li className="site-footer__item"><a href="https://wa.me/919882238158" target="_blank" rel="noopener noreferrer">WhatsApp Us</a></li>
+              <li className="site-footer__item"><a href="https://instagram.com/gharrat.in" target="_blank" rel="noopener noreferrer">Instagram</a></li>
             </ul>
           </div>
 
           <div className="site-footer__col">
             <p className="site-footer__heading">Get in touch</p>
-            <ul>
-              <li><a href="mailto:hello@gharrat.in">hello@gharrat.in</a></li>
-              <li>Palampur, Kangra Valley</li>
-              <li>Himachal Pradesh, India</li>
+            <ul className="site-footer__links">
+              <li className="site-footer__item"><a href="mailto:hello@gharrat.in">hello@gharrat.in</a></li>
+              <li className="site-footer__item">Palampur, Kangra Valley</li>
+              <li className="site-footer__item">Himachal Pradesh, India</li>
             </ul>
           </div>
         </div>
